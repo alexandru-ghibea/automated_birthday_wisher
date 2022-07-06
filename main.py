c0944e60ab0f21053a4d6b9fe4bb02182c433717
Alex <98416718+alexandru-ghibea@users.noreply.github.com>
2022-07-06 20:17:13 +0300

    Update main.py

--- main.py
+++ main.py
@@ -39,6 +39,8 @@ if check_birthday():
             with open(f"ready_to_send/letter_for_{name}.txt", "w") as created:
                 created.write(new_letter)
 # 4. Send the letter generated in step 3 to that person's email address.
+if not os.path.exists("ready_to_send"):
+    os.mkdir("ready_to_send")
 path = "./ready_to_send"
 email = "your email address"
 password_email = getpass.getpass("Password: ")
